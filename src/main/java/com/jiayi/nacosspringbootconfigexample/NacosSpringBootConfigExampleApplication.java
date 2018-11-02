cce05febb61baa00ef94845f61e5a0984a6ba40c
jiayi.nie <jiayi.nie@baozun.com>
2018-11-02 17:10:58 +0800

    init commit

--- src/main/java/com/jiayi/nacosspringbootconfigexample/NacosSpringBootConfigExampleApplication.java
+++ src/main/java/com/jiayi/nacosspringbootconfigexample/NacosSpringBootConfigExampleApplication.java
@@ -2,11 +2,9 @@ package com.jiayi.nacosspringbootconfigexample;
 
 import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableAutoConfiguration
 @NacosPropertySource(dataId = "example", autoRefreshed = true)
 public class NacosSpringBootConfigExampleApplication {
 
